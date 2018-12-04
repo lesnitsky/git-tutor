b14bd7edf653e3d35d755eb215b11152526fc7b9
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-12-05 02:11:57 +0300

    add prettier

--- index.js
+++ index.js
@@ -7,45 +7,50 @@ const Tags = {
 };
 
 async function formatCommits(commits) {
-    const formattedCommits = await Promise.all(commits.map(async commit => {
-        const message = commit.message();
-        const diffs = await commit.getDiff();
+    const formattedCommits = await Promise.all(
+        commits.map(async commit => {
+            const message = commit.message();
+            const diffs = await commit.getDiff();
 
-        const patches = _.flatten(await Promise.all(diffs.map(diff => diff.patches())));
+            const patches = _.flatten(await Promise.all(diffs.map(diff => diff.patches())));
 
-        const files = await Promise.all(patches.map(async patch => {
-            const hunks = await patch.hunks();
-            const lines = _.flatten(await Promise.all(hunks.map(hunk => hunk.lines())));
+            const files = await Promise.all(
+                patches.map(async patch => {
+                    const hunks = await patch.hunks();
+                    const lines = _.flatten(await Promise.all(hunks.map(hunk => hunk.lines())));
 
-            const diff = lines.map(line => ({
-                origin: String.fromCharCode(line.origin()),
-                content: line.content(),
-            }))
-            .filter(line => line.origin === '+' || line.origin === '-' || line.origin === ' ');
+                    const diff = lines
+                        .map(line => ({
+                            origin: String.fromCharCode(line.origin()),
+                            content: line.content(),
+                        }))
+                        .filter(line => line.origin === '+' || line.origin === '-' || line.origin === ' ');
+
+                    return {
+                        isAdded: patch.isAdded(),
+                        newPath: patch.newFile().path(),
+                        oldPath: patch.oldFile().path(),
+                        diff,
+                    };
+                })
+            );
 
             return {
-                isAdded: patch.isAdded(),
-                newPath: patch.newFile().path(),
-                oldPath: patch.oldFile().path(),
-                diff,
+                message,
+                files,
             };
-        }));
-
-        return {
-            message,
-            files,
-        };
-    }));
+        })
+    );
 
     return formattedCommits;
 }
 
 async function getCommits(history) {
     let resolve = () => {};
-    const p = new Promise(_ => resolve = _);
+    const p = new Promise(_ => (resolve = _));
     const commits = [];
 
-    history.on('commit', (c) => {
+    history.on('commit', c => {
         commits.unshift(c);
     });
 
@@ -59,16 +64,12 @@ function formatNewFile(file) {
     const ext = path.extname(file.newPath).substr(1);
     const text = file.diff.map(line => line.content).join('');
 
-    return '```' + ext +'\n' +
-        text +
-        '\n```';
+    return '```' + ext + '\n' + text + '\n```';
 }
 
 function formatDiff(file) {
     const text = file.diff.map(line => line.origin + ' ' + line.content).join('');
-    return '```diff\n' +
-        text +
-        '\n```';
+    return '```diff\n' + text + '\n```';
 }
 
 exports.render = async function render(pathToRepo) {
@@ -79,19 +80,25 @@ exports.render = async function render(pathToRepo) {
     const commits = await getCommits(history);
     const tutorialCommits = commits.filter(commit => !commit.message.startsWith(Tags.IGNORE));
 
-    const markdown = tutorialCommits.map(commit => [
-        commit.message,
-        commit.files.map(file => {
-            let formatted;
-            if (file.isAdded) {
-                formatted = formatNewFile(file);
-            } else {
-                formatted = formatDiff(file);
-            }
+    const markdown = tutorialCommits
+        .map(commit =>
+            [
+                commit.message,
+                commit.files
+                    .map(file => {
+                        let formatted;
+                        if (file.isAdded) {
+                            formatted = formatNewFile(file);
+                        } else {
+                            formatted = formatDiff(file);
+                        }
 
-            return `📄 ${file.newPath}\n` + formatted;
-        }).join('\n')
-    ].join('\n')).join('\n');
+                        return `📄 ${file.newPath}\n` + formatted;
+                    })
+                    .join('\n'),
+            ].join('\n')
+        )
+        .join('\n');
 
     return markdown;
 };
